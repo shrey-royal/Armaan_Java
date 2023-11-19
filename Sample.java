de1190797a54c819051c91eb37d8ea784cfde45a
Shrey K <srkkadia@gmail.com>
2023-11-19 15:52:04 +0530

    init

---
+++ Sample.java
@@ -0,0 +1,22 @@
+public class Sample {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("Welcome to Java Programming!");
+        //practice operators - arithmetic, relational, logical, bitwise, assignment, ternary, etc.
+    }
+}
+
+/*
+To compile and run the program, you need to open a command prompt and enter the following commands −
+
+-to compile the program
+C:\>javac Sample.java
+
+-to run the program
+C:\>java Sample
+
+the println() method is used to print the text inside the quotes, and at the end of the statement, a newline character is printed.
+
+if i use print() method instead of println() method, the text would be displayed on the same line.
+
+*/
